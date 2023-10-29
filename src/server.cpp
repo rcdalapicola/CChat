6069a3f79806b2f9394a999e11baa858dcf909c8
Rodolfo Coelho Dalapicola <rodolfo.dalapicola@gmail.com>
2023-10-29 21:41:28 +0100

    Allow multiple clients to connect to server

--- src/server.cpp
+++ src/server.cpp
@@ -4,6 +4,19 @@
 #include <unistd.h>     // close()
 #include "socket.h"
 #include <functional>
+#include <thread>
+#include <vector>
+
+void processIncomingMessages(const Socket* listenerSocket, const std::function<void(const char*)>& callbackFunction) {
+    ssize_t bytesRead;
+    static const int bufferSize = 256;
+    char buffer[bufferSize];
+
+    while ((bytesRead = recv(listenerSocket->socketConnection, buffer, bufferSize, 0)) > 0) {
+        buffer[bytesRead] = '\0'; 
+        callbackFunction(buffer);
+    }
+}
 
 int main() {
     Socket sck;
@@ -13,16 +26,23 @@ int main() {
         return 1;
     }
     std::cout << "Server connected." << std::endl;
-        
-    Socket tst(sck.getIncomingConnection());
 
-    auto messageCallback = [] (const char* message) {
-            std::cout << "Received message: \"" << message << "\"" << std::endl;
-    };
-    
-    tst.getIncomingMessages(messageCallback);
+    std::vector<std::thread> threadList;
+    int count = 0;
+    do {
+        Socket* listenerSocket = new Socket(sck.getIncomingConnection());
 
-    close(tst.socketConnection);
+        auto processMessage = [&listenerSocket] () {
+            auto messageCallback = [] (const char* message) {
+                std::cout << "Received message: \"" << message << "\"" << std::endl;
+            };
+            processIncomingMessages(listenerSocket, messageCallback);
+        };
+
+        threadList.emplace_back(processMessage);
+        count++;
+        std::cout << "Emplaced thread " << count << std::endl;
+    } while(1);
 
     std::cout << "Connection stablished and closed from server. " << std::endl;
 
